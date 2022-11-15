13fcb7e1f4969756664a3eb66c1d782061462063
Lestcreator <87945574+Lestcreator@users.noreply.github.com>
2022-11-15 18:28:30 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,25 +12,25 @@ global.APIKeys = {
 }
 
 global.self_mode = false
-global.autobio = true
+global.autobio = false
 global.msg_connected = false
 global.user_register = false
 global.anti_delete = false
 global.anticall = false
-global.auto_welcome_msg = true
-global.auto_leave_msg = true
+global.auto_welcome_msg = false
+global.auto_leave_msg = false
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.botname = "Alphabot"
+global.ownerNumberr = "+6283830960481"
+global.ownerbot = ['6283830960481@s.whatsapp.net','+79911505098@s.whatsapp.net'] // .owner
+global.ownername = "Kyzyen"
+global.botname = "Kyzyen"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
 global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.fake = "「  kyzyen  」\sad +79911505098"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
